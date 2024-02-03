80ab4946d00824d76ab1fc5ca83454763ae58d87
Emanuel Escudero <escudero.mdz@gmail.com>
2024-02-03 20:27:47 -0300

    Agrega el producto correctamente

--- 30-EcommerceV1.1-WebsocketHandlebars/src/public/js/index.js
+++ 30-EcommerceV1.1-WebsocketHandlebars/src/public/js/index.js
@@ -1,6 +1,8 @@
 const socket = io();
 
-const title = document.getElementById('title');
+// Form
+const formAddProduct = getElementById('formAddProduct');
+/* const title = document.getElementById('title');
 const description = document.getElementById('description');
 const price = document.getElementById('price');
 const thumbnails = document.getElementById('thumbnails');
@@ -9,25 +11,70 @@ const stock = document.getElementById('stock');
 const status = document.getElementById('status');
 const category = document.getElementById('category');
 const btnCreateProduct = document.getElementById('btnCreateProduct');
-const btnDeleteProduct = document.getElementById('btnDelete');
+const btnDeleteProduct = document.getElementById('btnDelete'); */
+
+// ul
+const productList = document.getElementById('products');
 
 
-btnCreateProduct.addEventListener('click', (e) => {
-  const product = {
-    title: title.value,
-    description: description.value,
-    price: price.value,
-    thumbnails: thumbnails.value,
-    code: code.value,
-    stock: stock.value,
-    status: status.value,
-    category: category.value,
+
+productList.addEventListener('click', async (e) => {
+  if (e.target.getAttribute('id') === 'btnDelete') {
+    const productId = e.target.parentElement.children[0].innerText.slice(8, 12);
+    try {
+      const response = await fetch(`/api/products/${productId}`, {
+        method: 'DELETE'
+      })
+      const { products } = await response.json()
+      socket.emit('delete-product', { productId, products });
+    } catch (error) {
+      console.log(error)
+    }
   }
+});
 
-  e.preventDefault();
-  socket.emit('newProductAdded', product);
+formAddProduct.addEventListener('submit', async (e) => {
+  e.preventDefault()
 
+  const newProduct = {};
+  const formData = new FormData(formAddProduct)
+  formData.forEach((value, key) => {
+    newProduct[key] = key === 'thumbnails'
+      ? Array.from(formData.getAll('thumbnails')).map(file => file.name)
+      : value.trim()
+  }
+  );
+  const response = await fetch('api/products', {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    // envio los datos del formulario al endopint
+    body: JSON.stringify(newProduct)
+  })
+  const { products } = await response.json()
+  socket.emit('new-product', { products });
 })
+
+socket.on('add-products', data => {
+  productList.innerHTML = ''
+  data.forEach(product => {
+    const productItem = document.createElement('li');
+    productItem.classList.add('card');
+    productItem.innerHTML = `
+        <h3>${product.title}</h3>
+        <p>${product.description}</p>
+        <p>Precio: $${product.price}</p>
+        <div class="btn-container">
+            <button class="btn-accept" data-id="btn-buy" id="buy${product.id}">COMPRAR</button>
+            <button class="btn-delete" data-id="btn-delete" id="delete${product.id}">ELIMINAR</button>
+            <button class="btn-cart">AGREGAR AL CARRITO</button>
+        </div>`;
+    productList.appendChild(productItem);
+  })
+})
+
+/* 
 
 socket.on('newProductAdded', (newProduct) => {
   const productList = document.getElementById('products');
@@ -47,4 +94,4 @@ socket.on('newProductAdded', (newProduct) => {
     <button id='btnDelete'>Delete product</button>
     `;
   productList.appendChild(newProductItem);
-})
+}) */
