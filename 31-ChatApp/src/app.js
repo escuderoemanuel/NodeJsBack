55af3993353e6fc25296f2c0706771958ea1fa3e
Emanuel Escudero <escudero.mdz@gmail.com>
2024-02-06 00:21:37 -0300

    31-ChatApp Updated

--- 31-ChatApp/src/app.js
+++ 31-ChatApp/src/app.js
@@ -5,31 +5,41 @@ const viewsRouter = require('./routes/views.router');
 const PORT = 8080;
 const sererMessage = `Server is running on port ${PORT}`;
 
+// Express
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Public Folder
 app.use(express.static(`${__dirname}/public`));
 
+// Handlebars
 app.engine('handlebars', handlebars.engine());
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'handlebars');
 
+// Routes
 app.use('/', viewsRouter);
 
-
-
-
+// Express
 const httpServer = app.listen(PORT, () => {
   console.log(sererMessage)
 })
 
-
+// Socket io
 const io = new Server(httpServer);
+
+let messages = [];
+
+
 io.on('connection', (socket) => {
   console.log(`User with id:${socket.id} connected...`);
 
-
+  socket.on('userMessage', (messageData) => {
+    messages.push(messageData);
+    io.emit('serverMessages', { messages: messages });
+  })
   socket.on('disconnect', () => {
     console.log(`User with id:${socket.id} disconnected...`);
   })
--- 31-ChatApp/src/public/js/chat.js
+++ 31-ChatApp/src/public/js/chat.js
@@ -1,1 +1,49 @@
 const socket = io();
+let username;
+
+// Elements
+const usernameFront = document.getElementById('usernameFront')
+const chatBox = document.getElementById("chatBox");
+const messagesLog = document.getElementById("messagesLog");
+
+// Events
+chatBox.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    if (chatBox.value.trim().length > 0) {
+      socket.emit("userMessage", {
+        username: username,
+        message: e.target.value,
+      });
+      e.target.value = "";
+    }
+  }
+})
+
+// Sockets Events
+socket.on("serverMessages", ({ messages }) => {
+  messagesLog.innerHTML = '';
+  messages.forEach(message => {
+    messagesLog.innerHTML += `<p><strong>${message.username}:</strong> ${message.message}</p>`;
+  })
+  messagesLog.scrollTop = messagesLog.scrollHeight;
+})
+
+
+// Login
+Swal.fire({
+  title: "👋 Hey, welcome! 😉",
+  text: "Enter your Username 👇",
+  input: "text",
+  allowOutsideClick: false,
+  inputValidator: (value) => {
+    if (!value) {
+      return "You need to write your Username!😠";
+    }
+  }
+}).then((result) => {
+  username = result.value;
+  usernameFront.innerHTML = `User: ${username}`;
+  socket.emit("newUser", username);
+});
+
+
