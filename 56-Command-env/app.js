1b1d53d1537fae0da51bd1bab2f559d2b2edbfa3
Emanuel Escudero <escudero.mdz@gmail.com>
2024-04-01 01:09:12 -0300

    56-Command-env Updated

--- 56-Command-env/app.js
+++ 56-Command-env/app.js
@@ -6,5 +6,5 @@ const app = express();
 
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on PORT: ${PORT}, ENVIRONMENT: ${ENVIRONMENT}`);
 });
--- 56-Command-env/config.js
+++ 56-Command-env/config.js
@@ -1,6 +1,13 @@
 const dotenv = require('dotenv');
 
-dotenv.config();
+
+const environment = 'DEVELOPMENT'
+
+// dotenv.config(); // Busca por default el .env
+
+dotenv.config({
+  path: environment === 'PRODUCTION' ? `${__dirname}/.env.production` : `${__dirname}/.env.development`
+});
 
 // console.log('port:', process.env.PORT);
 const { PORT, ENVIRONMENT, MONGO_URL } = process.env;
