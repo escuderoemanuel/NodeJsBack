b2ca719a46475cae196ea25db6c247a30d6a05f8
Emanuel Escudero <escudero.mdz@gmail.com>
2024-05-06 21:41:20 -0300

    83: logger

---
+++ 84-Clusters/src/app.js
@@ -0,0 +1,27 @@
+import cluster from 'cluster';
+import { cpus } from 'os';
+
+
+const numerosDeProcesadores = cpus().length;
+// console.log(numerosDeProcesadores)
+
+if (cluster.isPrimary) {
+  console.log('Proceso primario, generando proceso trabajador.')
+  for (let i = 0; i < numerosDeProcesadores; i++) {
+    cluster.fork();
+  }
+} else {
+  console.log('Proceso forkeado. isPrimary es false. Entonces soy un "worker"!')
+  console.log(`Soy un proceso worker con el id: ${process.pid}`)
+
+  app.get('/', (req, res) => {
+    res.send({ status: 'success', message: 'Petición atendida por un proceso worker...' })
+  })
+
+  app.listen(8080, () => {
+    'Listening on port 8080'
+  })
+
+}
+
+console.log(cluster.isPrimary)
