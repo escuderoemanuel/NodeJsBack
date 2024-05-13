a2dbf61b23210610889210398726fe01c8a3d0bc
Emanuel Escudero <escudero.mdz@gmail.com>
2024-05-13 00:05:01 -0300

    Updated

--- 84-Clusters/src/app.js
+++ 84-Clusters/src/app.js
@@ -1,12 +1,14 @@
 import cluster from 'cluster';
 import { cpus } from 'os';
 import express from 'express';
+import exp from 'constants';
 
 const numerosDeProcesadores = cpus().length;
 // console.log(numerosDeProcesadores)
 
 if (cluster.isPrimary) {
   console.log('Proceso primario, generando proceso trabajador.')
+
   for (let i = 0; i < numerosDeProcesadores; i++) {
     cluster.fork();
   }
@@ -16,13 +18,34 @@ if (cluster.isPrimary) {
   }) */
 
 } else {
+  console.log(`${process.pid} is primary? ${cluster.isPrimary}`)
   const port = 8080;
-  app.get('/', (req, res) => {
-    res.send({ status: 'success', message: 'Petición atendida por un proceso worker...' })
+  const app = express();
+
+  app.get('/operation/simple', (req, res) => {
+    let suma = 0;
+    for (let i = 0; i < 1_000_000; i++) {
+      suma += i;
+    }
+
+    console.log(`${process.pid}: suma: ${suma}`)
+
+    res.send({ status: 'success', payload: suma })
+  })
+
+  app.get('/operation/complex', (req, res) => {
+    let suma = 0;
+    for (let i = 0; i < 500_000_000; i++) {
+      suma += i;
+    }
+
+    console.log(`${process.pid}: suma: ${suma}`)
+
+    res.send({ status: 'success', payload: suma })
   })
 
   app.listen(port, () => {
-    `Running on port ${port} pid ${process.pid}`
+    console.log(`Running on port ${port} pid ${process.pid}`)
   })
 
 }
