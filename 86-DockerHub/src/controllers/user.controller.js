3a2f795ac65d6bed85a98ea1b931787034d0b429
Emanuel Escudero <escudero.mdz@gmail.com>
2024-05-15 15:41:15 -0300

    86-DockerHub: Published Image

--- 86-DockerHub/src/controllers/user.controller.js
+++ 86-DockerHub/src/controllers/user.controller.js
@@ -4,10 +4,10 @@ const createUser = async (req, res) => {
   const { cant } = req.query;
   try {
     const response = await userService.createUsersMock(cant);
-    return res.status(200).json({ users: response });
+    res.status(200).json({ users: response });
   } catch (error) {
     console.log('error:', error)
-    return res.status(200).json({
+    return res.status(400).json({
       errCode: -1,
       errMessage: "Error from server",
     });
@@ -20,7 +20,7 @@ const getUsers = async (req, res) => {
     return res.status(200).json({ users: response, pid: process.pid });
   } catch (error) {
     console.log('error:', error)
-    return res.status(200).json({
+    return res.status(400).json({
       errCode: -1,
       errMessage: "Error from server",
     });
