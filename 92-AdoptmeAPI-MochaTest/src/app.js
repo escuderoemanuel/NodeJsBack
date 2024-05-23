cbe53391999ced12d1315a70b7db77eda678bfe7
Emanuel Escudero <escudero.mdz@gmail.com>
2024-05-23 16:40:14 -0300

    Updated

---
+++ 92-AdoptmeAPI-MochaTest/src/app.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import swaggerUiExpress from 'swagger-ui-express';
+import swaggerJsDoc from 'swagger-jsdoc';
+import __dirname from './utils/index.js';
+
+import usersRouter from './routes/users.router.js';
+import petsRouter from './routes/pets.router.js';
+import adoptionsRouter from './routes/adoption.router.js';
+import sessionsRouter from './routes/sessions.router.js';
+
+//? Importación de variables de entorno
+import { config } from './config/environment.config.js';
+const { PORT, MONGO_URL } = config;
+
+const app = express();
+const connection = mongoose.connect(MONGO_URL)
+
+//? SWAGGER
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.1',
+    info: {
+      title: 'API Pets',
+      version: '1.0.0',
+      description: 'API to manage pet adoption',
+    },
+  },
+  apis: [`${__dirname}/../docs/**/*.yaml`],
+
+};
+
+const specs = swaggerJsDoc(swaggerOptions);
+app.use('/apidocs', swaggerUiExpress.serve, swaggerUiExpress.setup(specs));
+
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/users', usersRouter);
+app.use('/api/pets', petsRouter);
+app.use('/api/adoptions', adoptionsRouter);
+app.use('/api/sessions', sessionsRouter);
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
