c4538353c1c6f61be6ac1553c2df6113c8fb83d0
Emanuel Escudero <escudero.mdz@gmail.com>
2024-05-29 02:01:53 -0300

    94-AdoptmeAPI-Supertest

--- 94-AdoptmeAPI-MochaChaiAndSupertest/src/routes/pets.router.js
+++ 94-AdoptmeAPI-MochaChaiAndSupertest/src/routes/pets.router.js
@@ -5,6 +5,7 @@ const uploader = require('../utils/uploader.js');
 const router = Router();
 
 router.get('/',petsController.getAllPets);
+router.get('/:pid',petsController.getById);
 router.post('/',petsController.createPet);
 router.post('/withimage',uploader.single('image'), petsController.createPetWithImage);
 router.put('/:pid',petsController.updatePet);
--- 94-AdoptmeAPI-MochaChaiAndSupertest/src/test/routes/pets.router.test.js
+++ 94-AdoptmeAPI-MochaChaiAndSupertest/src/test/routes/pets.router.test.js
@@ -32,43 +32,54 @@ describe('Testing AdoptmeAPI', function () {
       }
     });
 
-    it('The POST endpoint "/api/pets" should create a pet correctly', async () => {
+    it('1. The POST endpoint "/api/pets" should create a pet correctly', async () => {
       const { statusCode, _body } = await requester.post('/api/pets').send(this.petMock);
       expect(_body).to.exist;
       expect(statusCode).to.be.equal(200);
       expect(_body.payload).to.have.property('_id');
     });
 
-    it('Al crear una mascota sólo con los datos elementales. Se debe corroborar que la mascota creada cuente con una propiedad adopted : false', async () => {
-      const { _body } = await requester.post('/api/pets').send(this.petMock);
-      expect(_body.payload).to.have.property('adopted')
-      expect(_body.payload.adopted).to.be.equal(false)
-      // expect(_body.payload).to.have.property('adopted', false); // Esto resume los dos anteriores
+    it('2. Al crear una mascota sólo con los datos elementales. Se debe corroborar que la mascota creada cuente con una propiedad adopted : false', async () => {
+      const { _body, statusCode } = await requester.post('/api/pets').send(this.petMock);
+      const responsePet = _body.payload;
+      expect(statusCode).to.be.equal(200)
+      expect(responsePet).to.have.property('adopted')
+      expect(responsePet.adopted).to.be.equal(false)
+      // expect(responsePet).to.have.property('adopted', false); // Esto resume los dos anteriores
     });
 
-    it('Si se desea crear una mascota sin el campo nombre, el módulo debe responder con un status 400', async () => {
+    it('3. Si se desea crear una mascota sin el campo nombre, el módulo debe responder con un status 400', async () => {
       const response = await requester.post('/api/pets').send({ specie: 'Dog', birthDate: '10-10-2020' });
       expect(response.statusCode).to.be.equal(400);
     });
 
-    it('Al obtener a las mascotas con el método GET, la respuesta debe tener los campos status y payload. Además, payload debe ser de tipo arreglo', async () => {
+    it('4. Al obtener a las mascotas con el método GET, la respuesta debe tener los campos status y payload. Además, payload debe ser de tipo arreglo', async () => {
       const newPet = await requester.post('/api/pets').send(this.petMock);
       const response = await requester.get('/api/pets');
       expect(response._body).to.have.property('status');
       expect(response._body).to.have.property('payload')
-      // expect(response._body.payload).to.be.an('array');
       expect(Array.isArray(response._body.payload)).to.be.true;
-      // expect(response._body).to.have.property('payload').that.is.an('array'); // Puede ser una alternativa
+      // expect(response._body.payload).to.be.an('array'); // Lo mismo que el anterior
+      // expect(Array.isArray(response._body.payload)).to.be.equal(true); // Lo mismo que el anterior
+      // expect(response._body).to.have.property('payload').that.is.an('array'); // Puede ser una alternativa para resumir los dos pasos
     });
 
-    it('El método PUT debe poder actualizar correctamente a una mascota determinada (esto se puede testear comparando el valor previo con el nuevo valor de la base de datos)', async () => {
+    it('5. El método PUT debe poder actualizar correctamente a una mascota determinada (esto se puede testear comparando el valor previo con el nuevo valor de la base de datos)', async () => {
+      const originalPet = await requester.post('/api/pets').send(this.petMock);
+      const pid = originalPet._body.payload._id;
+      await requester.put(`/api/pets/${pid}`).send({ name: 'Updated Coco' });
+      const updatedPet = await requester.get(`/api/pets/${pid}`);
+      expect(updatedPet.statusCode).to.be.equal(200);
+      expect(originalPet._body.payload.name).not.to.be.equal(updatedPet._body.payload.name)
+    })
+
+    it('El método DELETE debe poder borrar la última mascota agregada, ésto se puede alcanzar agregando a la mascota con un POST, tomando el id, borrando la mascota  con el DELETE, y luego corroborar si la mascota existe con un GET', async () => {
       const pet = await requester.post('/api/pets').send(this.petMock);
       const pid = pet._body.payload._id;
-      // console.log('pid', pid)
-      const putPet = await requester.put(`/api/pets/${pid}`).send({ name: 'Updated Coco', specie: 'Dog', birthDate: '10-10-2020' });
-      // const updatedPet = await requester.get(`/api/pets/${pid}`);
-      console.log('ok')
-      // expect(response._body.payload.name).to.be.equal('Updated Coco');
+      await requester.delete(`/api/pets/${pid}`);
+      const petDeleted = await requester.get(`/api/pets/${pid}`);
+      expect(petDeleted.statusCode).to.be.equal(400);
+      expect(petDeleted._body).not.to.have.property('payload') // Opcional para verificar que no exista un payload
     })
 
   });
