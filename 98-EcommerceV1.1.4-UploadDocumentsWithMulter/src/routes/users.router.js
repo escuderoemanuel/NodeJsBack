425927d95cbd0c9455fa3194e852524e54ecfc00
Emanuel Escudero <escudero.mdz@gmail.com>
2024-06-09 15:02:01 -0300

    Updated

--- 98-EcommerceV1.1.4-UploadDocumentsWithMulter/src/routes/users.router.js
+++ 98-EcommerceV1.1.4-UploadDocumentsWithMulter/src/routes/users.router.js
@@ -5,6 +5,7 @@ const usersRouter = Router();
 
 usersRouter.get('/', UsersController.getAll);
 usersRouter.get('/premium/:uid', UsersController.changeRole);
+usersRouter.get('/:uid/documents', UsersController)
 
 
 module.exports = usersRouter
